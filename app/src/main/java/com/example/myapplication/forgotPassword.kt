f4752bf25889aa4536444e5bfd92cd2b8040ece0
anandi savani <makanianandi21@gmail.com>
2022-11-02 18:05:21 -0400

    Comment Added

--- app/src/main/java/com/example/myapplication/forgotPassword.kt
+++ app/src/main/java/com/example/myapplication/forgotPassword.kt
@@ -11,7 +11,7 @@ import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
 
 class forgotPassword : AppCompatActivity() {
-//    create firebase object
+//    create firebase object testing
     var mAuth: FirebaseAuth? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
