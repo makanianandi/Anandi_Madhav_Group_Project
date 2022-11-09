e174a423685d1cdc9f9a4b3a55df2d7cf4d67b9c
Madhav2011 <madhavgabani@gmail.com>
2022-11-08 23:05:06 -0500

    Added Increment/Decrement of Quantity in productDetails.kt

--- app/src/main/java/com/example/myapplication/productDetails.kt
+++ app/src/main/java/com/example/myapplication/productDetails.kt
@@ -2,18 +2,20 @@ package com.example.myapplication
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 
 class productDetails : AppCompatActivity() {
 
+    var imgMinusButton: ImageButton? = null
+    var imgPlusButton:ImageButton? = null
     var imgProductFullView: ImageView? = null
     var txtProDetailsPrice: TextView? =null;
     var txtProDetailsName:TextView? = null;
     var txtProDetails:TextView? = null;
+    var txtProDetailsQty:TextView? = null;
+    var quantity = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,11 +24,42 @@ class productDetails : AppCompatActivity() {
         txtProDetailsName = findViewById<TextView>(R.id.txtProDetailsName)
         txtProDetailsPrice = findViewById<TextView>(R.id.txtProDetailsPrice)
         txtProDetails=findViewById<TextView>(R.id.txtProDetails)
+        txtProDetailsQty = findViewById<TextView>(R.id.txtProDetailsQty)
+        imgMinusButton = findViewById(R.id.imgMinusButton)
+        imgPlusButton = findViewById(R.id.imgPlusButton)
 
         txtProDetailsName!!.text = intent.getStringExtra("proName")
-        txtProDetailsPrice!!.text = intent.getStringExtra("proPrice")
+        txtProDetailsPrice!!.text = intent.getStringExtra("proPrice")?.drop(1)
         txtProDetails!!.text = intent.getStringExtra("proDesc")
         Glide.with(applicationContext).load(intent.getStringExtra("proImg")).into(imgProductFullView!!)
+
+        val price: String = txtProDetailsPrice!!.getText().toString()
+        val baseprice = price.toDouble()
+
+
+        imgPlusButton!!.setOnClickListener {
+            quantity++
+            displayQuantity()
+            val detailprice = baseprice * quantity
+            val setnewPrice = detailprice.toString()
+            txtProDetailsPrice!!.setText(setnewPrice)
+        }
+
+        imgMinusButton!!.setOnClickListener {
+            if (quantity == 1) {
+                Toast.makeText(
+                    this,
+                    "The Quantity Should be atleast 1",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                quantity--
+                displayQuantity()
+                val detailprice = baseprice * quantity
+                val setnewPrice = detailprice.toString()
+                txtProDetailsPrice!!.setText(setnewPrice)
+            }
+        }
 
         val btnAddToCart = findViewById<Button>(R.id.btnAddToCart)
         btnAddToCart.setOnClickListener {
@@ -34,6 +67,9 @@ class productDetails : AppCompatActivity() {
             startActivity(intent)
         }
 
+    }
+    private fun displayQuantity() {
+        txtProDetailsQty!!.setText(quantity.toString())
     }
 }
 
